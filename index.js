2e36269abf65d7982df49c6cc7352e473ede14c9
Doug Kintop <dkintop@hotmail.com>
2019-12-11 11:49:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event_name) {
+  let output = [];
+  for (let i = 0; i < array.length; i++) {
+    let message = `Thank you, ${array[i]}, for the wonderful ${event_name} gift!`;
+    output.push(message);
+  }
+  return output;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
