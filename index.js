c3ebbfc3a168554e1206cb5588b4c531a0797854
DBuckley0126 <dannyboy.msn@hotmail.com>
2019-12-16 18:31:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+
+function writeCards(names, event) {
+  let output = []
+  for(let i = 0; i < names.length; i++) {
+    output.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return output;
+}
+
+
+function countDown(number) {
+  let currentNumber = number
+  while(currentNumber >= 0) {
+    console.log(currentNumber)
+    currentNumber--
+  }
+}
