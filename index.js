9a35df56051a8e674086bf00970706f9ca5a1c06
Matthew Ream <matthewtimothyream@gmail.com>
2020-01-03 22:09:28 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+const names = ["Lisa", "Kaitlin", "Jan"];
+
+function writeCards(names, event_name) {
+    for (let i = 0; i < names.length; i++) {
+        console.log("Thank you, ${names[i]}, for the wonderful ${event_name} gift!");
+    }
+
+    return names;
+}
+
+writeCards(names, "surprise");
+
+
