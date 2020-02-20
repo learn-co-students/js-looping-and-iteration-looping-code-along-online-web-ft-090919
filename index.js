1ab31a1c5bde4091d49e9e20f0908fbeb61fe6b2
clonex10100 <clonex10100@gmail.com>
2020-02-20 05:00:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, celebration){
+    const messages = []
+    for(let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${celebration} gift!`);
+    }
+    return messages;
+}
+
+function countDown(x){
+    while(x >= 0){
+        console.log(x);
+        x--;
+    }
+}
